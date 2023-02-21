1b11c3880a256bda9cac4ac64af39dcc849b5b49
Dmitry Skirda <skirda_git@mail.ru>
2023-02-21 10:17:53 +0300

    create_static_getters

--- app/src/main/java/slirdad/calculator/MainActivity.java
+++ app/src/main/java/slirdad/calculator/MainActivity.java
@@ -14,29 +14,31 @@ public class MainActivity extends AppCompatActivity {
 
         final MainActivityViewHolder holder =
                 MainActivityViewHolderFactory.getMainActivityViewHolder(this);
+        final Calculator calculator = CalculatorFactory.getCalculator();
 
         holder.getMainTextView().setText("0"); //пока нет шареда
 
         for (Button button : holder.getNumButtons()) {
-            button.setOnClickListener(OnClickListenerFactory.getOnNumberButtonsClickListener());
+            button.setOnClickListener(
+                    OnClickListenerFactory.getOnNumberButtonsClickListener(calculator, holder));
         }
-        holder.getPlusButton().
-                setOnClickListener(OnClickListenerFactory.getOnPlusButtonClickListener());
-        holder.getMinusButton().
-                setOnClickListener(OnClickListenerFactory.getOnMinusButtonClickListener());
-        holder.getMultiplicationSignButton().
-                setOnClickListener(OnClickListenerFactory.getOnMultiplicationButtonClickListener());
-        holder.getDivisionSignButton().
-                setOnClickListener(OnClickListenerFactory.getOnDivisionButtonClickListener());
-        holder.getDeleteLastCharacterButton().
-                setOnClickListener(OnClickListenerFactory.getOnDeleteLastCharButtonClickListener());
-        holder.getSignChangeButton().
-                setOnClickListener(OnClickListenerFactory.getOnSignChangeButtonClickListener());
-        holder.getEqualMarkButton().
-                setOnClickListener(OnClickListenerFactory.getOnEqualMarkButtonClickListener());
-        holder.getAllCleanButton().
-                setOnClickListener(OnClickListenerFactory.getOnAllCleanButtonClickListener());
-        holder.getPointButton().
-                setOnClickListener(OnClickListenerFactory.getOnPointButtonClickListener());
+        holder.getPlusButton().setOnClickListener(
+                OnClickListenerFactory.getOnPlusButtonClickListener(calculator, holder));
+        holder.getMinusButton().setOnClickListener(
+                OnClickListenerFactory.getOnMinusButtonClickListener(calculator, holder, this));
+        holder.getMultiplicationSignButton().setOnClickListener(
+                OnClickListenerFactory.getOnMultiplicationButtonClickListener(calculator, holder));
+        holder.getDivisionSignButton().setOnClickListener(
+                OnClickListenerFactory.getOnDivisionButtonClickListener(calculator, holder));
+        holder.getDeleteLastCharacterButton().setOnClickListener(
+                OnClickListenerFactory.getOnDeleteLastCharButtonClickListener(calculator, holder));
+        holder.getSignChangeButton().setOnClickListener(
+                OnClickListenerFactory.getOnSignChangeButtonClickListener(calculator, holder));
+        holder.getEqualMarkButton().setOnClickListener(
+                OnClickListenerFactory.getOnEqualMarkButtonClickListener(calculator, holder));
+        holder.getAllCleanButton().setOnClickListener(
+                OnClickListenerFactory.getOnAllCleanButtonClickListener(calculator, holder));
+        holder.getPointButton().setOnClickListener(
+                OnClickListenerFactory.getOnPointButtonClickListener(calculator, holder));
     }
 }
--- app/src/main/java/slirdad/calculator/onClickListenerFactory.java
+++ app/src/main/java/slirdad/calculator/onClickListenerFactory.java
@@ -1,6 +1,6 @@
 package slirdad.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
 
 import slirdad.calculator.OnClickListeners.OnAllCleanButtonClickListener;
 import slirdad.calculator.OnClickListeners.OnDeleteLastCharButtonClickListener;
@@ -15,76 +15,43 @@ import slirdad.calculator.OnClickListeners.OnSignChangeButtonClickListener;
 
 class OnClickListenerFactory {
 
-    private static OnNumberButtonsClickListener onNumberButtonsClickListener;
-    private static OnPlusButtonClickListener onPlusButtonClickListener;
-    private static OnMinusButtonClickListener onMinusButtonClickListener;
-    private static OnMultiplicationButtonClickListener onMultiplicationButtonClickListener;
-    private static OnDivisionButtonClickListener onDivisionButtonClickListener;
-    private static OnDeleteLastCharButtonClickListener onDeleteLastCharButtonClickListener;
-    private static OnSignChangeButtonClickListener onSignChangeButtonClickListener;
-    private static OnEqualMarkButtonClickListener onEqualMarkButtonClickListener;
-    private static OnAllCleanButtonClickListener onAllCleanButtonClickListener;
-    private static OnPointButtonClickListener onPointButtonClickListener;
-
-
-
-    public OnClickListenerFactory(AppCompatActivity activity) {
-        MainActivityViewHolder viewHolder =
-                MainActivityViewHolderFactory.getMainActivityViewHolder(activity);
-
-        Calculator calculator = CalculatorFactory.getCalculator();
-
-        onNumberButtonsClickListener = new OnNumberButtonsClickListener(calculator, viewHolder);
-        onPlusButtonClickListener = new OnPlusButtonClickListener(calculator, viewHolder);
-        onMinusButtonClickListener = new OnMinusButtonClickListener(calculator, viewHolder, activity);
-        onMultiplicationButtonClickListener =
-                new OnMultiplicationButtonClickListener(calculator, viewHolder);
-        onDivisionButtonClickListener = new OnDivisionButtonClickListener(calculator, viewHolder);
-        onDeleteLastCharButtonClickListener =
-                new OnDeleteLastCharButtonClickListener(calculator, viewHolder);
-        onSignChangeButtonClickListener = new OnSignChangeButtonClickListener(calculator, viewHolder);
-        onEqualMarkButtonClickListener = new OnEqualMarkButtonClickListener(calculator, viewHolder);
-        onAllCleanButtonClickListener = new OnAllCleanButtonClickListener(calculator, viewHolder);
-        onPointButtonClickListener = new OnPointButtonClickListener(calculator, viewHolder);
+    public static OnNumberButtonsClickListener getOnNumberButtonsClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnNumberButtonsClickListener(calculator, holder);
     }
 
-    public static OnNumberButtonsClickListener getOnNumberButtonsClickListener() {
-        return onNumberButtonsClickListener;
+    public static OnPlusButtonClickListener getOnPlusButtonClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnPlusButtonClickListener(calculator, holder);
     }
 
-    public static OnPlusButtonClickListener getOnPlusButtonClickListener() {
-        return onPlusButtonClickListener;
+    public static OnMinusButtonClickListener getOnMinusButtonClickListener(Calculator calculator, MainActivityViewHolder holder, Context context) {
+        return new OnMinusButtonClickListener(calculator, holder, context);
     }
 
-    public static OnMinusButtonClickListener getOnMinusButtonClickListener() {
-        return onMinusButtonClickListener;
+    public static OnMultiplicationButtonClickListener getOnMultiplicationButtonClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnMultiplicationButtonClickListener(calculator, holder);
     }
 
-    public static OnMultiplicationButtonClickListener getOnMultiplicationButtonClickListener() {
-        return onMultiplicationButtonClickListener;
+    public static OnDivisionButtonClickListener getOnDivisionButtonClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnDivisionButtonClickListener(calculator, holder);
     }
 
-    public static OnDivisionButtonClickListener getOnDivisionButtonClickListener() {
-        return onDivisionButtonClickListener;
+    public static OnDeleteLastCharButtonClickListener getOnDeleteLastCharButtonClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnDeleteLastCharButtonClickListener(calculator, holder);
     }
 
-    public static OnDeleteLastCharButtonClickListener getOnDeleteLastCharButtonClickListener() {
-        return onDeleteLastCharButtonClickListener;
+    public static OnSignChangeButtonClickListener getOnSignChangeButtonClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnSignChangeButtonClickListener(calculator, holder);
     }
 
-    public static OnSignChangeButtonClickListener getOnSignChangeButtonClickListener() {
-        return onSignChangeButtonClickListener;
+    public static OnEqualMarkButtonClickListener getOnEqualMarkButtonClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnEqualMarkButtonClickListener(calculator, holder);
     }
 
-    public static OnEqualMarkButtonClickListener getOnEqualMarkButtonClickListener() {
-        return onEqualMarkButtonClickListener;
+    public static OnAllCleanButtonClickListener getOnAllCleanButtonClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnAllCleanButtonClickListener(calculator, holder);
     }
 
-    public static OnAllCleanButtonClickListener getOnAllCleanButtonClickListener() {
-        return onAllCleanButtonClickListener;
-    }
-
-    public static OnPointButtonClickListener getOnPointButtonClickListener() {
-        return onPointButtonClickListener;
+    public static OnPointButtonClickListener getOnPointButtonClickListener(Calculator calculator, MainActivityViewHolder holder) {
+        return new OnPointButtonClickListener(calculator, holder);
     }
 }
