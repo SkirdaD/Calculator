7fef9f7869d6c8dbd9d48450116751164048a2d6
Dmitry Skirda <skirda_git@mail.ru>
2023-03-01 22:31:49 +0300

    Feature(aboutApp)_create_fragment

--- app/src/main/java/slirdad/calculator/UI/MainActivity.java
+++ app/src/main/java/slirdad/calculator/UI/MainActivity.java
@@ -1,22 +1,28 @@
 package slirdad.calculator.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
-import slirdad.calculator.AboutAppActivity;
+import slirdad.calculator.AboutAppFragment;
 import slirdad.calculator.R;
 
 public class MainActivity extends AppCompatActivity {
+    AboutAppFragment aboutAppFragment;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_constraint);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        aboutAppFragment = new AboutAppFragment();
+
 
         final MainActivityViewHolder holder = new MainActivityViewHolder(this);
         final OnCLFactory factory = new OnCLFactory(holder);
@@ -46,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.about_app) {
-            startActivity(new Intent(this, AboutAppActivity.class));
+            fragmentTransaction.add(R.id.fragment, aboutAppFragment).commit();
         }
         return super.onOptionsItemSelected(item);
     }
