b1c5799422f3288bece7dc345e659f27f2b3f890
Dmitry Skirda <skirda_git@mail.ru>
2023-03-09 00:25:38 +0300

    fix_AboutAppLogicHolder_Strings

--- app/src/main/java/slirdad/calculator/AboutAppFragment/UI/AboutAppLogicHolder.java
+++ app/src/main/java/slirdad/calculator/AboutAppFragment/UI/AboutAppLogicHolder.java
@@ -14,17 +14,24 @@ class AboutAppLogicHolder {
     }
 
     void goToGitHub(@SuppressWarnings("unused") View view) {
+        String repositoryURL = "https://github.com/SkirdaD/Calculator";
+
         fragmentContext.startActivity
-                (new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/SkirdaD/Calculator")));
+                (new Intent(Intent.ACTION_VIEW, Uri.parse(repositoryURL)));
     }
 
     void sendEmail(@SuppressWarnings("unused") View view) {
+        String eMail = "skirda_git@mail.ru";
+        String eMailSubject = "Calculator.app";
+        String yourText = "Your text";
+        String sendEMail = "Send e-mail...";
+
         Intent emailIntent = new Intent(Intent.ACTION_SENDTO).
                 setData(Uri.parse("mailto:")).
-                putExtra(Intent.EXTRA_EMAIL, new String[]{"skirda_git@mail.ru"}).
-                putExtra(Intent.EXTRA_SUBJECT, "Calculator.app").
-                putExtra(Intent.EXTRA_TEXT, "Your text");
-        fragmentContext.startActivity(Intent.createChooser(emailIntent, "Send e-mail..."));
+                putExtra(Intent.EXTRA_EMAIL, new String[]{eMail}).
+                putExtra(Intent.EXTRA_SUBJECT, eMailSubject).
+                putExtra(Intent.EXTRA_TEXT, yourText);
+        fragmentContext.startActivity(Intent.createChooser(emailIntent, sendEMail));
     }
 
     void rateApplication(@SuppressWarnings("unused") View view) {
